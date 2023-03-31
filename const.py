3c1ea60a7a7e1ce580e0f9cb6c505b4880548113
dankeyy <dankeyy@protonmail.com>
2023-03-31 13:04:46 +0300

    forgot annoations and tuples exist

--- const.py
+++ const.py
@@ -16,26 +16,24 @@ class ConstHunter(ast.NodeVisitor):
         self.seen = set()
         super().__init__()
 
-    def visit_Assign(self, node):
-        for ass in node.targets:
-            ident = ass.id
 
-            if ident.isupper():
-                if ident in self.seen:
-                    try:
-                        tb = TracebackType(tb_next=None,
-                                           tb_frame=frame,
-                                           tb_lasti=0, # rekt
-                                           tb_lineno=node.lineno)
-                        raise SyntaxError("get const'd lol").with_traceback(tb)
+    def visit_Name(self, node):
+        ident = node.id
+        if ident.isupper():
+            if ident in self.seen:
+                try:
+                    tb = TracebackType(tb_next=None,
+                                        tb_frame=frame,
+                                        tb_lasti=0, # rekt
+                                        tb_lineno=node.lineno)
+                    raise SyntaxError("get const'd lol").with_traceback(tb)
 
-                    except:
-                        traceback.print_exc(limit=2)
-                        sys.exit(1)
+                except:
+                    traceback.print_exc(limit=2)
+                    sys.exit(1)
 
-                else:
-                    self.seen.add(ident)
-
+            else:
+                self.seen.add(ident)
 
 
 with open(frame.f_code.co_filename) as code:
--- test.py
+++ test.py
@@ -1,7 +1,12 @@
 import const
 
-BANANA = 2
+(BANANA, apple), (apples, ps) = (2, 1), (1,2)
+
+ORANGE: int = 3
 
 def f():
     global BANANA
     BANANA = 3
+
+# always catch the first, so this won't be caught until BANANA's second assignment is removed
+ORANGE = 1
